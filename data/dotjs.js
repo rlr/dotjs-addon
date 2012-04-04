e9df67877f6eec2850ee964b20e76615ff911564
canuckistani <jeff@canuckistani.ca>
2012-04-03 21:36:22 -0700

    a descriptive comment, tighter code, make sure we can load css even if there isn't any js code

--- data/dotjs.js
+++ data/dotjs.js
@@ -1,27 +1,24 @@
-// debugging
-var L = console.log, 
-D = function(o) { return JSON.stringify(o, null, '    '); };
-
+/*
+ * catch the 'load-scripts' event and inject the resilts into the current scope.
+ */
 (function() {
     self.port.on("load-scripts", function(msg) {
         if (msg.jquery) {
             eval(msg.jquery);
         }
-        else {
-            // jQuery is a hard requirement.
-            return;
-        }
+
         if (msg.js) {
             eval(msg.js);
         }
+
         if (msg.coffee) {
             (function() {
                 eval(msg.transpiler);
             }).call(window); // coffee-script.js assumes this === window
             eval(CoffeeScript.compile(msg.coffee));
         }
-        // needs testing...
-        if (msg.css) {            
+
+        if (msg.css) {
             var headNode = document.querySelector('head');
             var cssNode = document.createElement('style');
             cssNode.innerHTML = msg.css;
